ff013c8d8f827cf3ab43f17c273f3851a03fcd4f
손재준 <jaejoon.programmer@gmail.com>
2023-01-03 18:57:08 +0900

    11279번 priority queue

---
+++ 23_01/23_01_03.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <functional>
+#include <climits>
+#include <stack>
+#include <numeric>
+#include <deque>
+#define endl "\n"
+
+using namespace std;
+
+/* 11279번 최대 힙
+// 해설 : 우선순위 큐
+// url : https://www.acmicpc.net/problem/11279
+
+void input()
+{
+    int N;
+    cin >> N;
+
+    priority_queue<int> pq;
+
+    for(int i = 0 ; i < N ; i++)
+    {
+        int num;
+        cin >> num;
+
+        if(num == 0)
+        {
+            if(pq.empty())
+            {
+                cout << "0" << endl;
+            }
+            else
+            {
+                cout << pq.top() << endl;
+                pq.pop();
+            }
+        }
+        else
+        {
+            pq.emplace(num);
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    input();
+}
+*/
