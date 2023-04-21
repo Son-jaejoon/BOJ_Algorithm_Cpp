2883126d1101273b80f58db40795394fdf30b23e
손재준 <jaejoon.programmer@gmail.com>
2023-04-21 15:17:27 +0900

    9658번 게임 이론

---
+++ 23_04/23_04_21.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <functional>
+#include <climits>
+#include <stack>
+#include <numeric>
+#include <deque>
+#define endl "\n"
+
+using namespace std;
+
+/* 9658번 돌 게임 4
+// 해설 : 게임 이론
+// url : https://www.acmicpc.net/problem/9658
+
+int N;
+
+void input()
+{
+    cin >> N;
+}
+
+void solution()
+{
+    if(N % 7 == 1 || N % 7 == 3)
+        cout << "CY";
+    else
+        cout << "SK";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    input();
+    solution();
+}
+*/
